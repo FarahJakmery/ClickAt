20e9c6487c36d1839d9ff3430bd3fda69c2ddb98
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-04-13 12:58:39 +0300

    Create Fast Product & Fast Product Translation Models

---
+++ app/Models/Fastproduct.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
+use Carbon\Carbon;
+
+class Fastproduct extends Model implements TranslatableContract
+{
+    use HasFactory, Translatable;
+
+    protected $table = 'fastproducts';
+
+    protected $fillable = ['product_number', 'photo_name', 'min_price', 'max_price', 'counter', 'minutes', 'decreasing_value', 'product_date', 'expiry_date'];
+    public $translatedAttributes = ['name'];
+
+    /**
+     * The main_categories that belong to the fastProduct.
+     */
+    public function mcategories()
+    {
+        return $this->belongsToMany(Mcategory::class);
+    }
+}
