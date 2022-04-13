b3514f6355812a2434389d82a7d1c74a565f3d9d
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-04-13 12:42:58 +0300

    Add Api Routes To api.php File

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CodeproductController;
+use App\Http\Controllers\Api\FastproductController;
+use App\Http\Controllers\Api\McategoryController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +19,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
+    // Authentication Routes
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+});
+
+
+Route::middleware(['jwt.verify'])->group(function () {
+    // Main Categories Route
+    Route::resource('mcategories', McategoryController::class);
+    // Fast Selling Route
+    Route::get('fastSellingProduct', [FastproductController::class, 'index']);
+    Route::get('fastSellingProduct/{id}', [FastproductController::class, 'show']);
+    Route::get('mainCategory/{MainCategoriesID}/fastSellingProduct/{fastSellingProductId}', [FastproductController::class, 'showFastSellingProductBelogsToMainCategory']);
+    // Product Route
+    Route::get('products', [ProductController::class, 'index']);
+    Route::get('products/{id}', [ProductController::class, 'show']);
+    Route::get('mainCategory/{MainCategoriesID}/products/{productsId}', [ProductController::class, 'showProductBelogsToMainCategory']);
+    // Product with code Route
+    Route::resource('productWithCode', CodeproductController::class);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
 });
