79f3980ca6640813f36aeca110a36668ff5d80fe
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-04-13 12:27:02 +0300

    Fix All The links in those Files

--- resources/views/layouts/footer-scripts.blade.php
+++ resources/views/layouts/footer-scripts.blade.php
@@ -1,62 +1,62 @@
 <!-- JQuery min js -->
-<script src="../../assets/plugins/jquery/jquery.min.js"></script>
+<script src="{{ URL::asset('assets/plugins/jquery/jquery.min.js') }}"></script>
 
 <!-- Bootstrap Bundle js -->
-<script src="../../assets/plugins/bootstrap/js/popper.min.js"></script>
-<script src="../../assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="{{ URL::asset('assets/plugins/bootstrap/js/popper.min.js') }}"></script>
+<script src="{{ URL::asset('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 
 <!--Internal  Chart.bundle js -->
-<script src="../../assets/plugins/chart.js/Chart.bundle.min.js"></script>
+<script src="{{ URL::asset('assets/plugins/chart.js/Chart.bundle.min.js') }}"></script>
 
 <!-- Ionicons js -->
-<script src="../../assets/plugins/ionicons/ionicons.js"></script>
+<script src="{{ URL::asset('assets/plugins/ionicons/ionicons.js') }}"></script>
 
 <!-- Moment js -->
-<script src="../../assets/plugins/moment/moment.js"></script>
+<script src="{{ URL::asset('assets/plugins/moment/moment.js') }}"></script>
 
 <!--Internal Sparkline js -->
-<script src="../../assets/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
+<script src="{{ URL::asset('assets/plugins/jquery-sparkline/jquery.sparkline.min.js') }}"></script>
 
 <!-- Moment js -->
-<script src="../../assets/plugins/raphael/raphael.min.js"></script>
+<script src="{{ URL::asset('assets/plugins/raphael/raphael.min.js') }}"></script>
 
 <!--Internal Apexchart js-->
-<script src="../../assets/js/apexcharts.js"></script>
+<script src="{{ URL::asset('assets/js/apexcharts.js') }}"></script>
 
 <!-- Rating js-->
-<script src="../../assets/plugins/rating/jquery.rating-stars.js"></script>
-<script src="../../assets/plugins/rating/jquery.barrating.js"></script>
+<script src="{{ URL::asset('assets/plugins/rating/jquery.rating-stars.js') }}"></script>
+<script src="{{ URL::asset('assets/plugins/rating/jquery.barrating.js') }}"></script>
 
 <!--Internal  Perfect-scrollbar js -->
-<script src="../../assets/plugins/perfect-scrollbar/perfect-scrollbar.min-rtl.js"></script>
-<script src="../../assets/plugins/perfect-scrollbar/p-scroll-rtl.js"></script>
+<script src="{{ URL::asset('assets/plugins/perfect-scrollbar/perfect-scrollbar.min-rtl.js') }}"></script>
+<script src="{{ URL::asset('assets/plugins/perfect-scrollbar/p-scroll-rtl.js') }}"></script>
 
 <!-- Eva-icons js -->
-<script src="../../assets/js/eva-icons.min.js"></script>
+<script src="{{ URL::asset('assets/js/eva-icons.min.js') }}"></script>
 
 <!-- right-sidebar js -->
-<script src="../../assets/plugins/sidebar/sidebar-rtl.js"></script>
-<script src="../../assets/plugins/sidebar/sidebar-custom.js"></script>
+<script src="{{ URL::asset('assets/plugins/sidebar/sidebar-rtl.js') }}"></script>
+<script src="{{ URL::asset('assets/plugins/sidebar/sidebar-custom.js') }}"></script>
 
 <!-- Sticky js -->
-<script src="../../assets/js/sticky.js"></script>
-<script src="../../assets/js/modal-popup.js"></script>
+<script src="{{ URL::asset('assets/js/sticky.js') }}"></script>
+<script src="{{ URL::asset('assets/js/modal-popup.js') }}"></script>
 
 <!-- Left-menu js-->
-<script src="../../assets/plugins/side-menu/sidemenu.js"></script>
+<script src="{{ URL::asset('assets/plugins/side-menu/sidemenu.js') }}"></script>
 
 <!-- Internal Map -->
-<script src="../../assets/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="../../assets/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<script src="{{ URL::asset('assets/plugins/jqvmap/jquery.vmap.min.js') }}"></script>
+<script src="{{ URL::asset('assets/plugins/jqvmap/maps/jquery.vmap.usa.js') }}"></script>
 
 <!--Internal  index js -->
-<script src="../../assets/js/index.js"></script>
+<script src="{{ URL::asset('assets/js/index.js') }}"></script>
 
 <!-- Apexchart js-->
-<script src="../../assets/js/apexcharts.js"></script>
-<script src="../../assets/js/jquery.vmap.sampledata.js"></script>
+<script src="{{ URL::asset('assets/js/apexcharts.js') }}"></script>
+<script src="{{ URL::asset('assets/js/jquery.vmap.sampledata.js') }}"></script>
 
 <!-- custom js -->
-<script src="../../assets/js/custom.js"></script>
+<script src="{{ URL::asset('assets/js/custom.js') }}"></script>
 
 @yield('js')
