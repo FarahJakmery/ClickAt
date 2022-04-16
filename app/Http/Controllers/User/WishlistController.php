efffc17be7941f54af498df84294e56ef0b7ab2c
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-04-16 23:43:30 +0300

    Create Wishlist Controller For the User

---
+++ app/Http/Controllers/User/WishlistController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Wishlist;
+use Illuminate\Http\Request;
+
+class WishlistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = auth()->user()->wishlist()->latest()->get();
+        return view('User.Wishlist.wishlist', compact('products'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store()
+    {
+        if (!auth()->user()->wishlistHas(request('productId'))) {
+            auth()->user()->wishlist()->attach(request('productId'));
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Wishlist  $wishlist
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Wishlist $wishlist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Wishlist  $wishlist
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy()
+    {
+        auth()->user()->wishlist()->detach(request('productId'));
+    }
+}
