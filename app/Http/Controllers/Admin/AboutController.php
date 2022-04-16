cf151212d21a6421de1235508a3fb35fb130e1fe
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-04-16 23:15:09 +0300

    Craete About Controller and implement "index, store" Methodes

---
+++ app/Http/Controllers/Admin/AboutController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use App\Models\About;
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $abouts = About::translated()->get();
+        return view('Admin.AboutUs.about', compact('abouts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        Validator::make(
+            $request->all(),
+            [
+                'text_ar'  => ['required'],
+                'text_en'  => ['required'],
+            ]
+        );
+
+        $data = [
+            'ar' => [
+                'text'    => $request['text_ar'],
+            ],
+            'en' => [
+                'text'   => $request['text_en'],
+            ],
+        ];
+
+        About::create($data);
+
+        session()->flash('Add', 'تم إضافة عن كليكات بنجاح');
+        return redirect('/admin/about');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
