ed6bd153e0b44db5cf7a68347797c29ad78fe0f0
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-04-16 23:11:09 +0300

    Fix the links in those views

--- resources/views/weblayouts/footer-scripts.blade.php
+++ resources/views/weblayouts/footer-scripts.blade.php
@@ -1,18 +1,20 @@
 <!-- JS here -->
-<script src="js/vendor/jquery-3.5.0.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/bootstrap.bundle.min.js"></script>
-<script src="js/isotope.pkgd.min.js"></script>
-<script src="js/imagesloaded.pkgd.min.js"></script>
-<script src="js/jquery.magnific-popup.min.js"></script>
-<script src="js/owl.carousel.min.js"></script>
-<script src="js/jquery.odometer.min.js"></script>
-<script src="js/jquery.countdown.min.js"></script>
-<script src="js/jquery.appear.js"></script>
-<script src="js/slick.min.js"></script>
-<script src="js/ajax-form.js"></script>
-<script src="js/wow.min.js"></script>
-<script src="js/aos.js"></script>
-<script src="js/plugins.js"></script>
-<script src="js/main.js"></script>
+<script src="{{ URL::asset('Web/assets/js/vendor/jquery-3.5.0.min.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/popper.min.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/isotope.pkgd.min.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/imagesloaded.pkgd.min.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/jquery.magnific-popup.min.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/owl.carousel.min.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/jquery.odometer.min.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/jquery.countdown.min.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/jquery.appear.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/slick.min.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/ajax-form.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/wow.min.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/aos.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/plugins.js') }}"></script>
+<script src="{{ URL::asset('Web/assets/js/main.js') }}"></script>
+
+@yield('js')
