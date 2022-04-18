908db144f5365ca456f7de86887b65fbc43fb1ac
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-04-19 01:28:30 +0300

    Change the Max Value of the mobile numbers

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -52,7 +52,7 @@ class AuthController extends Controller
             'name'           => 'required|string|between:2,100',
             'email'          => 'required|string|email|max:100|unique:users',
             'password'       => 'required|string|min:6',
-            'mobile_number'  => 'required|string|min:8|max:11',
+            'mobile_number'  => 'required|string|min:8|max:25',
         ]);
         if ($validator->fails()) {
             return response()->json($validator->errors()->toJson(), 400);
