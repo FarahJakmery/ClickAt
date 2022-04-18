d6989ac9df81941605f72f72a553ce2430a649a4
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-04-19 01:36:36 +0300

    Add some Routes to api.php file

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CodeproductController;
 use App\Http\Controllers\Api\FastproductController;
 use App\Http\Controllers\Api\McategoryController;
+use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\WishlistController;
 use Illuminate\Http\Request;
@@ -34,28 +35,32 @@ Route::group(['middleware' => 'api', 'prefix' => 'auth'], function ($router) {
 });
 
 
+// Main Categories Route
+Route::resource('mcategories', McategoryController::class);
+Route::get('mainCategory/{MainCategoryID}/fastSellingProducts', [McategoryController::class, 'showAllFastSellingProductsBelogsToMainCategory']);
+Route::get('mainCategory/{MainCategoryID}/products', [McategoryController::class, 'showAllProductsBelogsToMainCategory']);
+
+// Fast Selling Route
+Route::get('fastSellingProduct', [FastproductController::class, 'index']);
+Route::get('fastSellingProduct/{id}', [FastproductController::class, 'show']);
+
+// Product Route
+Route::get('products', [ProductController::class, 'index']);
+Route::get('products/{id}', [ProductController::class, 'show']);
+
+// Product with code Route
+Route::resource('productWithCode', CodeproductController::class);
+
+
 Route::middleware(['jwt.verify'])->group(function () {
-    // Main Categories Route
-    Route::resource('mcategories', McategoryController::class);
-    Route::get('mainCategory/{MainCategoryID}/fastSellingProducts', [McategoryController::class, 'showAllFastSellingProductsBelogsToMainCategory']);
-    Route::get('mainCategory/{MainCategoryID}/products', [McategoryController::class, 'showAllProductsBelogsToMainCategory']);
-
-    // Fast Selling Route
-    Route::get('fastSellingProduct', [FastproductController::class, 'index']);
-    Route::get('fastSellingProduct/{id}', [FastproductController::class, 'show']);
-
-    // Product Route
-    Route::get('products', [ProductController::class, 'index']);
-    Route::get('products/{id}', [ProductController::class, 'show']);
-
-    // Product with code Route
-    // Route::resource('productWithCode', CodeproductController::class);
-    Route::get('/user-profile', [AuthController::class, 'userProfile']);
-
     // Wishlist Routes
     Route::get('Wishlist', [WishlistController::class, 'index']);
     Route::post('AddFastProductToWishlist', [WishlistController::class, 'storeFastProduct']);
     Route::post('AddProductToWishlist', [WishlistController::class, 'storeProduct']);
     Route::post('AddCodeToWishlist', [WishlistController::class, 'storeCode']);
     Route::delete('Wishlistdestroy', [WishlistController::class, 'destroy'])->name('apiwishlist.destroy');
+
+    // Order Routes
+    Route::get('orders', [OrderController::class, 'index'])->name('orders');
+    Route::post('addOrder', [OrderController::class, 'store'])->name('addOrder');
 });
