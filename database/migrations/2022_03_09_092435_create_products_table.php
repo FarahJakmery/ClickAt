6edfa052352bf6f795a477a479adf371162a05ae
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-04-27 13:27:09 +0300

    Change The Type of The "url" column

--- database/migrations/2022_03_09_092435_create_products_table.php
+++ database/migrations/2022_03_09_092435_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('photo_name');
-            $table->string('url');
+            $table->text('url');
             $table->timestamps();
         });
     }
