a4d7986514be99a27e363fb5fd5255623a90c323
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-04-27 14:32:19 +0300

    Create a Search Controller For the User

--- app/Http/Controllers/User/SearchController.php
+++ app/Http/Controllers/User/SearchController.php
@@ -14,42 +14,38 @@ class SearchController extends Controller
     public function search(Request $request)
     {
         if ($request->searchWord !== null && $request->mainCategoryId == "null") {
-            $products = Product::whereTranslationLike('product_name', "%$request->searchWord%")->get();
+
             $fastProducts = Fastproduct::whereTranslationLike('name', "%$request->searchWord%")->get();
+            $products = Product::whereTranslationLike('product_name', "%$request->searchWord%")->get();
             $codes = Codeproduct::whereTranslationLike('codeproduct_name', "%$request->searchWord%")->get();
-            $array = [
-                'products'     => $products,
-                'fastProducts' => $fastProducts,
-                'codes' => $codes,
-            ];
-            return $array;
-        } elseif ($request->searchWord == null && $request->mainCategoryId !== "null") {
-            $products = Product::whereHas('maincategories', function ($query) use ($request) {
-                $query->where('id', $request->mainCategoryId);
-            })->get();
+            return view('User.Search.searchResults', compact('fastProducts', 'products', 'codes'));
+        }
+        //
+        elseif ($request->searchWord == null && $request->mainCategoryId !== "null") {
+
             $fastProducts = Fastproduct::whereHas('mcategories', function ($query) use ($request) {
                 $query->where('id', $request->mainCategoryId);
             })->get();
-            $array = [
-                'products'     => $products,
-                'fastProducts' => $fastProducts,
-            ];
-            return $array;
-        } elseif ($request->searchWord !== null && $request->mainCategoryId !== "null") {
-            $products = Product::whereTranslationLike('product_name', "%$request->searchWord%")->whereHas('maincategories', function ($query) use ($request) {
+            $products = Product::whereHas('maincategories', function ($query) use ($request) {
                 $query->where('id', $request->mainCategoryId);
             })->get();
+            return view('User.Search.searchResults', compact('fastProducts', 'products'));
+        }
+        //
+        elseif ($request->searchWord !== null && $request->mainCategoryId !== "null") {
+
             $fastProducts = Fastproduct::whereTranslationLike('name', "%$request->searchWord%")->whereHas('mcategories', function ($query) use ($request) {
                 $query->where('id', $request->mainCategoryId);
             })->get();
-            $array = [
-                'products'     => $products,
-                'fastProducts' => $fastProducts,
-            ];
-            return $array;
-        } elseif ($request->searchWord == null && $request->mainCategoryId == "null") {
-            $f = "There is not any thing";
-            return $f;
+            $products = Product::whereTranslationLike('product_name', "%$request->searchWord%")->whereHas('maincategories', function ($query) use ($request) {
+                $query->where('id', $request->mainCategoryId);
+            })->get();
+            return view('User.Search.searchResults', compact('fastProducts', 'products'));
+        }
+        //
+        elseif ($request->searchWord == null && $request->mainCategoryId == "null") {
+
+            return view('User.Search.searchResults');
         }
     }
 }
