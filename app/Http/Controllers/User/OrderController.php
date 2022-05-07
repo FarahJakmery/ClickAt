66aca1ec83efb0e8b5c0e1d4b39c43359a8784db
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-05-07 13:09:48 +0300

    Create And Implement "getOrderItems" Function in this Controller

--- app/Http/Controllers/User/OrderController.php
+++ app/Http/Controllers/User/OrderController.php
@@ -17,74 +17,14 @@ class OrderController extends Controller
      */
     public function index()
     {
-        $orders = Order::where('user_id', 4)->get();
-        $orderItems = OrderItem::all();
-        return view('User.Order.orders', compact('orders', 'orderItems'));
+        $orders = Order::where('user_id', Auth::user()->id)->get();
+        return view('User.Order.orders', compact('orders'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function getOrderItems($id)
     {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        $orderItems = OrderItem::where('order_id', $id)->get();
+        return json_encode($orderItems);
     }
 }
