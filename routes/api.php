b5a1f48beb8ed57b914e62945c662445c29a8ebe
FarahJakmery <74063674+FarahJakmery@users.noreply.github.com>
2022-06-12 09:33:02 +0300

    Make Changes On api.php File

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CartController;
 use App\Http\Controllers\Api\CodeproductController;
 use App\Http\Controllers\Api\FastproductController;
 use App\Http\Controllers\Api\McategoryController;
 use App\Http\Controllers\Api\OrderController;
+use App\Http\Controllers\Api\PaymentController;
 use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Api\WishlistController;
 use Illuminate\Http\Request;
@@ -59,6 +61,19 @@ Route::middleware(['jwt.verify'])->group(function () {
     Route::post('AddProductToWishlist', [WishlistController::class, 'storeProduct']);
     Route::delete('Wishlistdestroy', [WishlistController::class, 'destroy'])->name('apiwishlist.destroy');
 
+    // Add To Cart Route
+    Route::post('addToCart', [CartController::class, 'addToCart'])->name('addToCart');
+    // Cart Route
+    Route::get('Cart', [CartController::class, 'show'])->name('Cart');
+    // Update Cart Route
+    Route::put('update_cart', [CartController::class, 'update'])->name('update');
+    // Remove From Cart Route
+    Route::delete('remove_From_Cart', [CartController::class, 'removeFromCart'])->name('removeFromCart');
+    // Checkout Route
+    Route::get('check_Quantites', [PaymentController::class, 'checkQuantites'])->name('checkQuantites');
+    Route::post('checkout', [PaymentController::class, 'checkout'])->name('checkout');
+    // Change Order Status After Payment
+    Route::post('/paytaps_response/{id}', [PaymentController::class, 'paytaps_response']);
     // Order Routes
     Route::get('orders', [OrderController::class, 'index'])->name('orders');
     Route::post('addOrder', [OrderController::class, 'store'])->name('addOrder');
